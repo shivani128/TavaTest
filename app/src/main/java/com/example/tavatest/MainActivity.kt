8741bff6b853eb5adb35fcb88f4e06272a9026a2
shivani128 <95333678+shivani128@users.noreply.github.com>
2021-12-03 20:02:24 +0530

    Add all my files

--- app/src/main/java/com/example/tavatest/MainActivity.kt
+++ app/src/main/java/com/example/tavatest/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.tavatest
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.Handler
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import com.example.tavatest.databinding.ActivityMainBinding
+@RequiresApi(Build.VERSION_CODES.M)
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var hanlder=Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setUI()
+
+
+        hanlder.postDelayed({
+          setUI()
+        },2000)
+    }
+
+
+    fun setUI() {
+
+        binding.AmbA.setOnClickListener {
+            binding.a.setBackgroundColor(getColor(R.color.green))
+            binding.b.setBackgroundColor(getColor(R.color.white))
+            binding.c.setBackgroundColor(getColor(R.color.white))
+            binding.d.setBackgroundColor(getColor(R.color.white))
+        }
+        binding.BAMB.setOnClickListener {
+            binding.a.setBackgroundColor(getColor(R.color.white))
+            binding.b.setBackgroundColor(getColor(R.color.green))
+            binding.c.setBackgroundColor(getColor(R.color.white))
+            binding.d.setBackgroundColor(getColor(R.color.white))
+        }
+        binding.CAMB.setOnClickListener {
+            binding.a.setBackgroundColor(getColor(R.color.white))
+            binding.b.setBackgroundColor(getColor(R.color.white))
+            binding.c.setBackgroundColor(getColor(R.color.green))
+            binding.d.setBackgroundColor(getColor(R.color.white))
+        }
+        binding.DABM.setOnClickListener {
+            binding.a.setBackgroundColor(getColor(R.color.white))
+            binding.b.setBackgroundColor(getColor(R.color.white))
+            binding.c.setBackgroundColor(getColor(R.color.white))
+            binding.d.setBackgroundColor(getColor(R.color.green))
+        }
     }
 }
